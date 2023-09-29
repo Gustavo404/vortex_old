9fb5c81b69570bc22628b3ef92bb75f2bb815371
Gustavo404 <gustavo404.marcio@gmail.com>
2023-09-29 13:51:43 -0300

    detecção de args para uso simplificado

--- vortex.sh
+++ vortex.sh
@@ -5,22 +5,53 @@ echo # pula uma linha por conta do banner
 echo "Bem vindo ao Vortex"
 echo # pula uma linha para ficar mais bonito
 
-# coleta de dados input output ip user e pass
-echo "Digite o nome do arquivo de entrada: "
-read input
+# Verifica se foram fornecidos argumentos de linha de comando
+if [ $# -eq 0 ]; then
+    # coleta de dados input output ip user e pass
+    echo "Digite o nome do arquivo de entrada: "
+    read input
 
-echo "Digite o nome do arquivo de saida: "
-read output
+    echo "Digite o nome do arquivo de saida: "
+    read output
 
-echo "Digite o ip do servidor telnet: "
-read ip
+    echo "Digite o ip do servidor telnet: "
+    read ip
 
-echo "Digite o usuario do servidor telnet: "
-read user
+    echo "Digite o usuario do servidor telnet: "
+    read user
 
-echo "Digite a senha do servidor telnet: "
-read pass
-
+    echo "Digite a senha do servidor telnet: "
+    read pass
+else
+    # Processa as opções da linha de comando
+    while getopts "ip:user:pass:input:output:" opt; do
+        case $opt in
+            ip)
+                ip="$OPTARG"
+                ;;
+            user)
+                user="$OPTARG"
+                ;;
+            input)
+                input="$OPTARG"
+                ;;
+            output)
+                output="$OPTARG"
+                ;;
+            output)
+                output="$OPTARG"
+                ;;
+            \?)
+                echo "Opção inválida: -$OPTARG" >&2
+                exit 1
+                ;;
+            :)
+                echo "A opção -$OPTARG requer um argumento." >&2
+                exit 1
+                ;;
+        esac
+    done
+fi
 # verifica se todas as variaveis foram preenchidas
 if [ -z "$input" ] || [ -z "$output" ] || [ -z "$ip" ] || [ -z "$user" ] || [ -z "$pass" ]
 then
@@ -34,19 +65,22 @@ if [ ! -f "$input" ]; then
     exit 1
 fi
 
-# Verifica se o arquivo de entrada possui apenas números e tabs (X Y Z)
-if [[ "$input" =~ ^[0-9\t]+$ ]]; then
-    echo "O arquivo de entrada está no padrão Z Y X"
-    echo "Executando o tsunami.sh -i"	
-    bash tsunami.sh -i $input
-# Verifica se o arquivo de entrada possui /
-elif [[ "$input" == *"/"* ]]; then
-    echo "O arquivo de entrada possui está no padrão X/Y/Z"
-    echo "Executando o tsunami.sh -t"
-    bash tsunami.sh -t $input
+# Lê a primeira linha do arquivo
+first_line=$(head -n 1 "$input")
+
+# Expressão regular para verificar o padrão "Números Tab Números Tab Números"
+pattern1="^[0-9]+\t[0-9]+\t[0-9]+$"
+
+# Expressão regular para verificar o padrão "Números/Números/Números"
+pattern2="^[0-9]+/[0-9]+/[0-9]+$"
+
+if [[ "$first_line" =~ $pattern1 ]]; then
+  echo "A primeira linha corresponde ao padrão 1: $first_line"
+  # Ação 1: Coloque aqui o código para a ação 1
+elif [[ "$first_line" =~ $pattern2 ]]; then
+  echo "A primeira linha corresponde ao padrão 2: $first_line"
+  # Ação 2: Coloque aqui o código para a ação 2
 else
-    # Se não for nenhum dos casos anteriores, assume que são comandos do telnet
-    echo "O arquivo de entrada possui comandos do telnet"
-    echo "Executando o oxygen.expect"
-    expect oxygen/oxygen.expect $ip $user $pass $input $output | see $output
+  echo "A primeira linha não corresponde a nenhum padrão: $first_line"
+  # Ação 3: Coloque aqui o código para a ação 3
 fi
