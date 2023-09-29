8e9c6c9977810b70679c2c26b2b91c9588ee1620
Gustavo404 <gustavo404.marcio@gmail.com>
2023-09-29 11:17:06 -0300

    adição do codigo bruto de vortex.sh

--- vortex.sh
+++ vortex.sh
@@ -1,2 +1,48 @@
 #!/bin/bash
+
 cat banner.txt
+
+# coleta de dados
+echo "Digite o nome do arquivo de entrada: "
+read input
+
+echo "Digite o nome do arquivo de saida: "
+read output
+
+echo "Digite o ip do servidor telnet: "
+read ip
+
+echo "Digite o usuario do servidor telnet: "
+read user
+
+echo "Digite a senha do servidor telnet: "
+read pass
+
+# verifica se todas as variaveis foram preenchidas
+if [ -z "$input" ] || [ -z "$output" ] || [ -z "$ip" ] || [ -z "$user" ] || [ -z "$pass" ]
+then
+    echo "Alguma variavel nao foi preenchida"
+    exit 1
+fi
+
+# Verifica se o arquivo de entrada existe
+if [ ! -f "$input" ]; then
+    echo "O arquivo de entrada não existe"
+    exit 1
+fi
+
+# Verifica se o arquivo de entrada possui apenas números e tabs (12 87  3)
+if [[ "$input" =~ ^[0-9\t]+$ ]]; then
+    echo "O arquivo de entrada está no padrão Z Y X"
+    echo "Executando o tsunami.sh -i"	
+    bash tsunami.sh -i $input
+# Verifica se o arquivo de entrada possui /
+elif [[ "$input" == *"/"* ]]; then
+    echo "O arquivo de entrada possui está no padrão X/Y/Z"
+    echo "Executando o tsunami.sh -t"
+    bash tsunami.sh -t $input
+else
+    # Se não for nenhum dos casos anteriores, assume que são comandos do telnet
+    echo "O arquivo de entrada possui comandos do telnet"
+    echo "Executando o oxygen.expect"
+    bash oxygen/oxygen.expect $ip $user $pass $input $output | see $output
