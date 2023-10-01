ab3307fea69e5ddc528093f7493200e5f08940c8
gustavo404 <gustavo404.marcio@gmail.com>
2023-10-01 13:14:10 -0300

    Melhoria na estrutura do codigo

--- install.sh
+++ install.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 
-install() {
-    echo "Instalando..."
-    apt install expect git -y
-    # apt install python3-pip -y
-    # clona os repositorios do github que fazem parte do projeto (dependencias)
-    git clone https://github.com/Gustavo404/obsidian
-    git clone https://github.com/Gustavo404/oxygen
-    git clone https://github.com/Gustavo404/tsunami
+# Função para instalar as dependências do projeto
+instalar_dependencias() {
+  echo "Instalando dependências..."
+  apt install expect git -y
+  # apt install python3-pip -y
+  # clona os repositórios do GitHub que fazem parte do projeto (dependências)
+  git clone https://github.com/Gustavo404/obsidian
+  git clone https://github.com/Gustavo404/oxygen
+  git clone https://github.com/Gustavo404/tsunami
 }
 
-reinstall() {
-    echo "Re-instalando..."
-    rm -rf obsidian oxygen tsunami
-    install
+# Função para reinstalar as dependências do projeto
+reinstalar_dependencias() {
+  echo "Reinstalando dependências..."
+  rm -rf obsidian oxygen tsunami
+  instalar_dependencias
 }
 
+# Processamento das opções da linha de comando
 while getopts "ir" opt; do
   case "$opt" in
     i)
-      install
+      instalar_dependencias
       ;;
     r)
-      reinstall
+      reinstalar_dependencias
       ;;
     \?)
       echo "Uso: $0 [-i] [-r]" >&2
@@ -31,9 +34,11 @@ while getopts "ir" opt; do
   esac
 done
 
-shift $((OPTIND-1))
-
+# Verificação de argumentos inválidos
 if [ "$#" -ne 0 ]; then
   echo "Argumentos inválidos."
   exit 1
 fi
+
+# Finalização do script
+echo "Script finalizado"
